a491269c824db72032a156c2ab00e52cf854394e
fepz <franpaez@gmail.com>
2015-10-06 11:02:22 -0300

    Multiple plots.

--- process_results.py
+++ process_results.py
@@ -1,70 +1,48 @@
 import pandas as pd
 import numpy as np
-import itertools
 import matplotlib.pyplot as plt
 
-# These are the "Tableau 20" colors as RGB.    
+# These are the "Tableau 20" colors as RGB.
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
              (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
              (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
              (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
              (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
              
+labels_text = {"kruskal":"Kruskal", "prim":"Prim", "prim_2h":"Prim 2-Heap",
+               "prim_binomial":"Prim Binomial", "prim_fibonacci":"Prim Fibonacci",
+               "prim_3h":"Prim 3-Heap", "kruskal_sorted1":"Kruskal (Sorted 1)",
+               "kruskal_sorted2":"Kruskal (Sorted 2)",
+               "prim_binomial_nx":"Prim Binomial (nx)",
+               "prim_2h_nx":"Prim 2-Heap (nx)",
+               "prim_3h_nx":"Prim 3-Heap (nx)",
+               "prim_fibonacci_nx":"Prim Fibonacci (nx)"}
+               
+all_methods = list(labels_text.keys())
+prim_methods = [m for m in all_methods if m.startswith("prim")]
+prim_methods_nx = [m for m in all_methods if m.endswith("_nx")]
+kruskal_methods = [m for m in all_methods if m.startswith("kruskal")]
 
-def graph(df, title, save_file):        
-    labels_text = {"kruskal":"Kruskal", "prim":"Prim", "prim_bh":"Prim 2-Heap",
-                   "prim_binomial":"Prim Binomial", "prim_fibonacci":"Prim Fibonacci",
-                   "prim_th":"Prim 3-Heap", "kruskal_sorted1":"Kruskal (Sorted 1)",
-                   "kruskal_sorted2":"Kruskal (Sorted 2)"}
-
+def graph(df, title, save_file, methods=[]):        
     fig, ax = plt.subplots(figsize=(10,7.5))
     
-    plt.xticks(fontsize=12)    
+    plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.xlabel("Density", fontsize=16)
     plt.ylabel(r'Time ($\mu$ secs)', fontsize=16)
     plt.title(title, fontsize=18)
     plt.grid(True)
         
-    labels = []
-    for rank, v in enumerate(df.groupby(["Algorithm"])):    
+    for rank, v in enumerate(df.groupby(["Algorithm"])):
         algorithm, group = v[0], v[1]
-        group_agg = group.groupby(["Density"]).mean()["Time"]        
-        ax = group_agg.plot(ax=ax, kind="line", lw=2.5, ms=7, color=tableau20[rank])
-        labels.append(labels_text[algorithm])
+        if algorithm in methods:
+            group_agg = group.groupby(["Density"]).mean()["Time"]        
+            ax = group_agg.plot(ax=ax, kind="line", lw=2.5, ms=7, color=tableau20[rank], label=labels_text[algorithm])
     
-    lines, _ = ax.get_legend_handles_labels()
-    ax.legend(lines, labels, loc='best')
+    plt.legend(loc="best")
     
     ax.get_figure().savefig(save_file)
 
-
-def graph_old(df, save_file):
-    # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
-    for i in range(len(tableau20)):    
-        r, g, b = tableau20[i]    
-        tableau20[i] = (r / 255., g / 255., b / 255.)
-
-    marker = itertools.cycle(('D', '+', 's', 'o', '*','x'))
-    
-    fig, ax = plt.subplots(figsize=(10,7.5))
-    
-    #plt.xticks(range(0, 91, 10), [str(x) + "%" for x in range(0, 91, 10)], fontsize=14)    
-    plt.xticks(fontsize=14)    
-    plt.yticks(fontsize=14)    
-    
-    labels = []
-    for rank, v in enumerate(df.groupby(["Algorithm"])):    
-        algorithm, group = v[0], v[1]
-        group_agg = group.groupby(["Density"]).mean()["Time"]        
-        ax = group_agg.plot(ax=ax, kind="line", x="Density", y="Time", lw=2.5,
-                            marker=next(marker), ms=7, color=tableau20[rank])
-        labels.append(algorithm)
-    lines, _ = ax.get_legend_handles_labels()
-    ax.legend(lines, labels, loc='best')
-    #plt.show()
-    ax.get_figure().savefig(save_file)
-    
 
 def graph2(df):
     """ remove outliers """
@@ -93,12 +71,33 @@ def main():
         r, g, b = tableau20[i]    
         tableau20[i] = (r / 255., g / 255., b / 255.)
     
-    graph(data[data.Edges < 110], "100 Edges", "100.pdf") # 100 edges    
-    graph(data[(data.Edges < 1100) & (data.Edges > 110)], "1000 Edges", "1000.pdf") # 1000 edges
-    graph(data[(data.Edges < 11000) & (data.Edges > 1100)], "10000 Edges", "10000.pdf") # 10000 edges
+    # Graph kruskal methods    
+    graph(data[data.Edges < 110], "100 Edges - Kruskal", "100K.pdf", kruskal_methods) # 100 edges    
+    graph(data[(data.Edges < 1100) & (data.Edges > 110)], "1000 Edges - Kruskal", "1000K.pdf", kruskal_methods) # 1000 edges
+    graph(data[(data.Edges < 11000) & (data.Edges > 1100)], "10000 Edges - Kruskal", "10000K.pdf", kruskal_methods) # 10000 edges    
+    
+    # Graph all Prim methods
+    graph(data[data.Edges < 110], "100 Edges - Prim", "100P.pdf", prim_methods) # 100 edges    
+    graph(data[(data.Edges < 1100) & (data.Edges > 110)], "1000 Edges - Prim", "1000P.pdf", prim_methods) # 1000 edges
+    graph(data[(data.Edges < 11000) & (data.Edges > 1100)], "10000 Edges - Prim", "10000P.pdf", prim_methods) # 10000 edges
     #graph(data[(data.Edges < 110000) & (data.Edges > 11000)], "100000.pdf") # 100000 edges
     #graph(data[(data.Edges < 1100000) & (data.Edges > 110000)], "1000000.pdf") # 1000000 edges
     
+    # Graph all Prim methods
+    graph(data[data.Edges < 110], "100 Edges - Prim (nx)", "100Pnx.pdf", prim_methods_nx) # 100 edges    
+    graph(data[(data.Edges < 1100) & (data.Edges > 110)], "1000 Edges - Prim (nx)", "1000Pnx.pdf", prim_methods_nx) # 1000 edges
+    graph(data[(data.Edges < 11000) & (data.Edges > 1100)], "10000 Edges - Prim (nx)", "10000Pnx.pdf", prim_methods_nx) # 10000 edges
+    #graph(data[(data.Edges < 110000) & (data.Edges > 11000)], "100000.pdf") # 100000 edges
+    #graph(data[(data.Edges < 1100000) & (data.Edges > 110000)], "1000000.pdf") # 1000000 edges
+    
+    # Graph Prim (nx) methods, Prim and Kruskal
+    ppk = []
+    ppk.extend(prim_methods_nx)
+    ppk.append("kruskal")
+    ppk.append("prim")
+    graph(data[data.Edges < 110], "100 Edges - Prim (nx)", "100Ppk.pdf", ppk) # 100 edges    
+    graph(data[(data.Edges < 1100) & (data.Edges > 110)], "1000 Edges - Prim (nx)", "1000Ppk.pdf", ppk) # 1000 edges
+    graph(data[(data.Edges < 11000) & (data.Edges > 1100)], "10000 Edges - Prim (nx)", "10000Ppk.pdf", ppk) # 10000 edges
 
 if __name__ == '__main__':
     main()
