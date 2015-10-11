e5339514abe2ae33ed1ce6134155cc304dd39777
fepz <franpaez@gmail.com>
2015-10-11 19:44:55 -0300

    Generate bulk graphs.

--- aycc.py
+++ aycc.py
@@ -27,20 +27,27 @@ def kruskal(graph, edges=[], sort=False):
 
     # Initialize n sets, each containing a different element of N
     uf = ds.DisjointSets(graph.nodes())
+    
+    while_count = 0
+    if_count = 0
 
     # greedy loop
     tn = 0   # number of edges in the set
     en = 0
     while tn < n - 1:
+        while_count += 1
         e = edges[en]
         en += 1
         u = uf.find(e[0])
         v = uf.find(e[1])
 
         if u != v:
+            if_count += 1
             mst.append(e)
             tn += 1
             uf.merge(u, v)
+
+    #print(while_count, if_count, if_count/while_count)
 
     return mst
 
@@ -144,7 +151,8 @@ def prim_generic_heap_nx(graph, heap):
         
         # add the edge to the mst if the cost is know
         if graph.node[v]['e_v'] is not None:
-            mst.append((v, graph.node[v]['e_v']))
+            v1 = graph.node[v]['e_v']
+            mst.append((v, graph.node[v]['e_v'], {'weight':graph[v][v1]["weight"]}))
         
         # update the weights (costs) of the edges associated with min_node
         for n in graph.neighbors(v):
@@ -159,10 +167,11 @@ def prim_generic_heap_nx(graph, heap):
 def get_args():
     """ Parse arguments from the command line """
     parser = ArgumentParser()
-    parser.add_argument("--graphpath", help="Directory with graphs", default=".", type=str)    
+    parser.add_argument("--graphpath", help="Directory with graphs/results", default=".", type=str)    
     parser.add_argument("--numreps", help="Number of test reps per method", default=10, type=int)
     parser.add_argument("--graph", help="Generate graphs", action="store_true")
-    parser.add_argument("--report", help="Generate report", action="store_true")
+    parser.add_argument("--reportgraph", help="Generate report and complete graphs for the report", action="store_true")
+    parser.add_argument("--test", help="Run the test", action="store_true")
     return parser.parse_args()
     
     
@@ -179,67 +188,68 @@ def test_mst(graph):
     #clockt = time.clock() - clockt
     #tests["kruskal_nx"] = [mst.edges(data=True), clockt]
     
-    clockt = time.clock()
-    mst = kruskal(graph)
-    clockt = time.clock() - clockt
-    results["kruskal"] = [mst, clockt, len(mst)]
-    
     edges = graph.edges(data=True)
     edges.sort(key=lambda edge: edge[2]['weight'])
     clockt = time.clock()
     mst = kruskal(graph, edges)
     clockt = time.clock() - clockt
-    results["kruskal_sorted1"] = [mst, clockt, len(mst)]
+    results["kruskal_sorted1"] = [mst, clockt]
     
     clockt = time.clock()
     mst = kruskal(graph, sort=True)
     clockt = time.clock() - clockt
-    results["kruskal_sorted2"] = [mst, clockt, len(mst)]
+    results["kruskal_sorted2"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim(graph)
     clockt = time.clock() - clockt
-    results["prim"] = [mst, clockt, len(mst)]
+    results["prim"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim_generic_heap(graph, dh.Heap(2))
     clockt = time.clock() - clockt
-    results["prim_2h"] = [mst, clockt, len(mst)]
+    results["prim_2h"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim_generic_heap_nx(graph, dh.Heap(2))
     clockt = time.clock() - clockt
-    results["prim_2h_nx"] = [mst, clockt, len(mst)]
+    results["prim_2h_nx"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim_generic_heap(graph, dh.Heap(3))
     clockt = time.clock() - clockt
-    results["prim_3h"] = [mst, clockt, len(mst)]
+    results["prim_3h"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim_generic_heap_nx(graph, dh.Heap(3))
     clockt = time.clock() - clockt
-    results["prim_3h_nx"] = [mst, clockt, len(mst)]
+    results["prim_3h_nx"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim_generic_heap(graph, bh.BinomialHeap())
     clockt = time.clock() - clockt
-    results["prim_binomial"] = [mst, clockt, len(mst)]
+    results["prim_binomial"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim_generic_heap_nx(graph, bh.BinomialHeap())
     clockt = time.clock() - clockt
-    results["prim_binomial_nx"] = [mst, clockt, len(mst)]
+    results["prim_binomial_nx"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim_generic_heap(graph, fh.FibonacciHeap())
     clockt = time.clock() - clockt
-    results["prim_fibonacci"] = [mst, clockt, len(mst)]
+    results["prim_fibonacci"] = [mst, clockt]
     
     clockt = time.clock()
     mst = prim_generic_heap_nx(graph, fh.FibonacciHeap())
     clockt = time.clock() - clockt
-    results["prim_fibonacci_nx"] = [mst, clockt, len(mst)]
+    results["prim_fibonacci_nx"] = [mst, clockt]
+    
+    # calculates length and sum weight for each mst
+    for method, result in results.items():
+        rmst = result[0]
+        result.append(len(rmst))
+        result.append(sum(edge[2]['weight'] for edge in rmst))
     
     return [nx.number_of_nodes(graph), 
             nx.number_of_edges(graph), 
@@ -249,61 +259,70 @@ def test_mst(graph):
 
 def main():    
     args = get_args()
-    
+
     # Output file path
     save_file_path = "{0}/test-result.txt".format(args.graphpath)
     
-    # Search for all the edge files in grappath directory
-    edge_files = glob.glob("{0}/*.edgelist".format(args.graphpath))
-    
-    # Verifies that there are .edgelist files in graphpath
-    if not edge_files:
-        path_string = "current" if args.graphpath == "." else args.graphpath
-        print("Error: No *.edgelist files found in {0} directory!".format(path_string), file=sys.stderr)
-        return
-       
-    with open(save_file_path, "w") as save_file:        
-        save_file.write("Test\tFile\tNodes\tEdges\tDensity\tAlgorithm\tTime\n")
+    if args.test:
+        # Search for all the edge files in grappath directory
+        edge_files = glob.glob("{0}/*.edgelist".format(args.graphpath))
         
-        for edge_file in edge_files:            
-            print("Testing {0} ...".format(edge_file), file=sys.stdout)
+        # Verifies that there are .edgelist files in graphpath
+        if not edge_files:
+            path_string = "current" if args.graphpath == "." else args.graphpath
+            print("Error: No *.edgelist files found in {0} directory!".format(path_string), file=sys.stderr)
+            return        
+           
+        with open(save_file_path, "w") as save_file:        
+            save_file.write("Test\tFile\tNodes\tEdges\tDensity\tAlgorithm\tTime\n")
             
-            for rep in range(args.numreps):
-                # Read graph from the file
-                graph = nx.read_weighted_edgelist(edge_file, nodetype=int)
-
-                # Test the mst methods
-                result = test_mst(graph)
+            for edge_file in edge_files:            
+                print("Testing {0} ...".format(edge_file), file=sys.stdout)
                 
-                # Verifies that all the methods give the same mst length
-                mst_lengths = []
-                for method, method_results in result[3].items():
-                    mst_lengths.append((method, method_results[2]))
-                lmst = mst_lengths[0]
-                for r in mst_lengths[1:]:
-                    if r[1] != lmst[1]:
-                        print("ERROR!!! {0} |mst|={1}, {2} |mst|={3}".format(lmst[0],lmst[1],r[0],r[1]), file=sys.stderr)
-                        return
-                
-                # Write the results into the output file
-                for method, method_results in result[3].items():
-                    save_file.write("{0}\t{1}\t{2}\t{3}\t{4:0.1}\t".format(rep, os.path.basename(edge_file), result[0], result[1], result[2]))
-                    save_file.write("{0}\t{1}\n".format(method, method_results[1]))
-                save_file.write("\n")
-             
-            # Flush last rep results into the output file
-            save_file.flush()
+                for rep in range(args.numreps):
+                    # Read graph from the file
+                    graph = nx.read_weighted_edgelist(edge_file, nodetype=int)
+    
+                    # Test the mst methods
+                    result = test_mst(graph)
+                    
+                    # Verifies that all the methods give the same mst length
+                    mst_lengths = []
+                    for method, method_results in result[3].items():
+                        mst_lengths.append((method, method_results[2]))
+                    lmst = mst_lengths[0]
+                    for r in mst_lengths[1:]:
+                        if r[1] != lmst[1]:
+                            print("ERROR!!! {0} |mst|={1}, {2} |mst|={3}".format(lmst[0],lmst[1],r[0],r[1]), file=sys.stderr)                        
+                            return
+                            
+                    # Verifies that all the methods give the same mst weight
+                    mst_weights = []
+                    for method, method_results in result[3].items():
+                        mst_weights.append((method, method_results[3]))
+                    wmst = mst_weights[0]
+                    for r in mst_weights[1:]:
+                        if r[1] != wmst[1]:
+                            print("ERROR!!! {0} wmst={1}, {2} wmst={3}".format(wmst[0],wmst[1],r[0],r[1]), file=sys.stderr)                        
+                            return                
+                    
+                    # Write the results into the output file
+                    for method, method_results in result[3].items():
+                        save_file.write("{0}\t{1}\t{2}\t{3}\t{4:0.1}\t".format(rep, os.path.basename(edge_file), result[0], result[1], result[2]))
+                        save_file.write("{0}\t{1}\n".format(method, method_results[1]))
+                    save_file.write("\n")
+                 
+                # Flush last rep results into the output file
+                save_file.flush()
             
-    # Generate the graphs
-    if args.graph:
-        print("Generating PDFs...", file=sys.stdout)
-        pr.generate_graphs(save_file_path, args.graphpath)
-              
-    # Generate a report in CSV
-    if args.report:
-        print("Generating CSV report...", file=sys.stdout)
-        report_save_file_path = "{0}/test-report.csv".format(args.graphpath)        
-        pr.generate_simple_report_csv(save_file_path, report_save_file_path)
+        # Generate the graphs for this test
+        if args.graph:
+            print("Generating PDFs...", file=sys.stdout)
+            pr.generate_graphs(save_file_path, args.graphpath)
+                      
+    if args.reportgraph:
+        print("Generating PDFs..")
+        pr.graphX(args.graphpath)
     
     print("Done!", file=sys.stdout)
         
--- process_results.py
+++ process_results.py
@@ -1,45 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# These are the "Tableau 20" colors as RGB.
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
-
 # Labels
-labels_text = {"kruskal":"Kruskal (no sorted)", 
-               "kruskal_sorted1":"Kruskal (sorted 1)",
-               "kruskal_sorted2":"Kruskal (sorted 2)",
-               "prim":"Prim", 
-               "prim_2h":"Prim 2-Heap",
-               "prim_3h":"Prim 3-Heap",
-               "prim_binomial":"Prim Binomial", 
-               "prim_fibonacci":"Prim Fibonacci",                   
-               "prim_binomial_nx":"Prim Binomial (nx)",
-               "prim_2h_nx":"Prim 2-Heap (nx)",
-               "prim_3h_nx":"Prim 3-Heap (nx)",
-               "prim_fibonacci_nx":"Prim Fibonacci (nx)"}
+labels_text = {"kruskal_sorted1":"Kruskal (KS1)",
+               "kruskal_sorted2":"Kruskal (KS2)",
+               "prim":"PrimArr", 
+               "prim_2h":"Prim2H (A)", #Prim 2-Heap
+               "prim_3h":"Prim3H (A)", #Prim 3-Heap
+               "prim_binomial":"PrimBi (A)", # Binomial
+               "prim_fibonacci":"PrimFib (A)", # Fibonacci                   
+               "prim_binomial_nx":"PrimBi",
+               "prim_2h_nx":"Prim2H",
+               "prim_3h_nx":"Prim3H",
+               "prim_fibonacci_nx":"PrimFib"}
                
 all_methods = list(labels_text.keys())
 prim_methods = [m for m in all_methods if m.startswith("prim")]
 prim_methods_nx = [m for m in all_methods if m.endswith("_nx")]
 kruskal_methods = [m for m in all_methods if m.startswith("kruskal")]
 
-
-def graph(df, title, save_file, methods=[], colors=[], variant="Density"):
-    if not colors:
-        colors = tableau20
-       
+def graph(df, title, save_file, methods=[], colors=[], variant="Density", log=False):
     fig, ax = plt.subplots(figsize=(10,7.5))
     
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.xlabel("Density", fontsize=16)
-    plt.ylabel(r'Time ($\mu$ secs)', fontsize=16)
+    plt.ylabel(r'Time (secs)', fontsize=16)
     plt.title(title, fontsize=18)
     plt.grid(True)
+    
+    if log:
+        plt.xscale("log")
+        plt.yscale("log")
         
     for rank, v in enumerate(df.groupby(["Algorithm"])):
         algorithm, group = v[0], v[1]
@@ -52,12 +44,7 @@ def graph(df, title, save_file, methods=[], colors=[], variant="Density"):
     
     plt.close(fig)
         
-    
-def scatter(df):
-    for algorithm, data in df.groupby(["Algorithm"]):
-        data.plot(kind="scatter", x="Density", y="Time")
-        
-        
+            
 def generate_simple_report(file, save_file_path, methods=[]):   
     """ Generate a simple report """    
     import numpy as np
@@ -78,7 +65,35 @@ def generate_simple_report_csv(file, save_file_path):
 def generate_graphs(file, save_path):
     """ Graph results in file. """
     data = pd.read_csv(file, sep='\t')
+    generate_graphs2(data, save_path)
     
+#    # These are the "Tableau 20" colors as RGB.
+#    tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
+#                 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+#                 (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+#                 (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+#                 (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]                
+#    
+#    # Scale the RGB values to the [0, 1] range, which is the format matplotlib 
+#    # accepts.    
+#    for i in range(len(tableau20)):    
+#        r, g, b = tableau20[i]    
+#        tableau20[i] = (r / 255., g / 255., b / 255.)
+#        
+#    graph(data, "Kruskal", "{0}/kruskal.pdf".format(save_path), kruskal_methods, tableau20)
+#    graph(data, "Prim", "{0}/prim.pdf".format(save_path), prim_methods, tableau20)
+#    graph(data, "Prim (nx)", "{0}/primnx.pdf".format(save_path), prim_methods_nx, tableau20)
+#    
+#    # Graph Prim (nx) methods, Prim and Kruskal
+#    ppk = []
+#    ppk.extend(prim_methods_nx)
+#    ppk.append("kruskal_sorted1")
+#    ppk.append("kruskal_sorted2")
+#    ppk.append("prim")
+#    graph(data, "PPK", "{0}/ppk.pdf".format(save_path), ppk, tableau20)
+    
+    
+def generate_graphs2(data, save_path, filename_prefix=""):
     # These are the "Tableau 20" colors as RGB.
     tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
                  (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
@@ -92,14 +107,103 @@ def generate_graphs(file, save_path):
         r, g, b = tableau20[i]    
         tableau20[i] = (r / 255., g / 255., b / 255.)
         
-    graph(data, "Kruskal", "{0}/kruskal.pdf".format(save_path), kruskal_methods, tableau20)
-    graph(data, "Prim", "{0}/prim.pdf".format(save_path), prim_methods, tableau20)
-    graph(data, "Prim (nx)", "{0}/primnx.pdf".format(save_path), prim_methods_nx, tableau20)
+    graph(data, "Kruskal", "{0}/{1}kruskal.pdf".format(save_path,filename_prefix), kruskal_methods, tableau20)
+    graph(data, "Prim (A)", "{0}/{1}primA.pdf".format(save_path,filename_prefix), prim_methods, tableau20)
+    graph(data, "Prim", "{0}/{1}prim.pdf".format(save_path,filename_prefix), prim_methods_nx, tableau20)
     
     # Graph Prim (nx) methods, Prim and Kruskal
     ppk = []
     ppk.extend(prim_methods_nx)
-    ppk.append("kruskal")
+    ppk.append("kruskal_sorted1")
+    ppk.append("kruskal_sorted2")
     ppk.append("prim")
-    graph(data, "PPK", "{0}/ppk.pdf".format(save_path), ppk, tableau20)    
+    graph(data, "Prim & Kruskal", "{0}/{1}pk.pdf".format(save_path, filename_prefix), ppk, tableau20)    
     
+    
+def generate_graphs_by_density(path):
+    import os
+    import os.path
+    
+    # These are the "Tableau 20" colors as RGB.
+    tableau20 = [(174, 199, 232), (255, 127, 14), (31, 119, 180), (255, 187, 120),    
+                 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+                 (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+                 (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+                 (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]                
+    
+    # Scale the RGB values to the [0, 1] range, which is the format matplotlib 
+    # accepts.    
+    for i in range(len(tableau20)):    
+        r, g, b = tableau20[i]    
+        tableau20[i] = (r / 255., g / 255., b / 255.)
+
+    # Iterate over each subdir in path, and add any test-result file to the 
+    # data-frame df
+    dfs = []    
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in [f for f in filenames if f.startswith("test-result")]:            
+            dfs.append(pd.read_csv(dirpath+"\\"+filename, sep='\t'))            
+    df = pd.concat(dfs)
+
+    kruskal_heap = ["kruskal_sorted1", "kruskal_sorted2","prim_2h_nx","prim_3h_nx","prim_binomial_nx","prim_fibonacci_nx"]        
+
+    # Graph Prim (nx) methods, Prim and Kruskal -- all densities
+    graph(df, "Edges", "{0}/edges.pdf".format(path), kruskal_heap, tableau20, variant="Edges", log=True)
+    
+    # One graph for each density
+    for rank, v in enumerate(df.groupby(["Density"])):
+        density, group = v[0], v[1]
+        graph(group, "Density {0}".format(str(density)), "{0}/Cruces/{1}_pc.pdf".format(path,str(density)), kruskal_heap, tableau20, variant="Edges", log=True)
+        
+        
+def graphX(path):
+    import os
+    import os.path
+    
+    # These are the "Tableau 20" colors as RGB.
+    tableau20 = [(174, 199, 232), (255, 127, 14), (31, 119, 180), (255, 187, 120),    
+                 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+                 (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+                 (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+                 (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]                
+    
+    # Scale the RGB values to the [0, 1] range, which is the format matplotlib 
+    # accepts.    
+    for i in range(len(tableau20)):    
+        r, g, b = tableau20[i]    
+        tableau20[i] = (r / 255., g / 255., b / 255.)
+
+    # Iterate over each subdir in path, and add any test-result file to the 
+    # data-frame df
+    dfs = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in [f for f in filenames if f.startswith("test-result")]:            
+            dfs.append(pd.read_csv(dirpath+"\\"+filename, sep='\t'))            
+    df = pd.concat(dfs)
+
+    kruskal_heap = ["kruskal_sorted1", "kruskal_sorted2","prim_2h_nx","prim_3h_nx","prim_binomial_nx","prim_fibonacci_nx"]        
+
+    # Graph Prim (nx) methods, Prim and Kruskal -- all densities
+    graph(df, "Edges", "{0}/edges.pdf".format(path), kruskal_heap, tableau20, variant="Edges", log=True)
+    
+    # One graph for each density
+    for rank, v in enumerate(df.groupby(["Density"])):
+        density, group = v[0], v[1]
+        graph(group, "Density {0}".format(str(density)), "{0}/{1}-cruce-pk.pdf".format(path,str(density)), kruskal_heap, tableau20, variant="Edges", log=True)
+        
+    # Graphs by number of edges
+    df_100 = df[df["File"].str.contains("graph_100_")]
+    df_1000 = df[df["File"].str.contains("graph_1000_")]
+    df_10000 = df[df["File"].str.contains("graph_10000_")]
+    df_100000 = df[df["File"].str.contains("graph_100000_")]
+    df_1000000 = df[df["File"].str.contains("graph_1000000_")]
+    
+    generate_graphs2(df_100, path, "100-")
+    generate_graphs2(df_1000, path, "1000-")
+    generate_graphs2(df_10000, path, "10000-")
+    generate_graphs2(df_100000, path, "100000-")   
+    generate_graphs2(df_1000000, path, "1000000-")
+    
+    
+    
+
